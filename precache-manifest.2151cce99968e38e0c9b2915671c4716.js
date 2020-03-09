5e7e057d856161e38ec4f410a05e7fe0199ac9a6
unknown <igoreduardosavatin@gmail.com>
2020-03-09 00:24:11 -0300

    Updates

---
+++ precache-manifest.2151cce99968e38e0c9b2915671c4716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d72df81ce83c7a7c34daee21891801",
+    "url": "/igor-savatin.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fc3d9e1d69bbd9362b6d",
+    "url": "/igor-savatin.github.io/monsters-rolodex/static/css/main.418921e5.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/igor-savatin.github.io/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/igor-savatin.github.io/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc3d9e1d69bbd9362b6d",
+    "url": "/igor-savatin.github.io/monsters-rolodex/static/js/main.33fcd9c3.chunk.js"
+  },
+  {
+    "revision": "9231245b37a228cdabe0",
+    "url": "/igor-savatin.github.io/monsters-rolodex/static/js/runtime-main.858638cc.js"
+  }
+]);
